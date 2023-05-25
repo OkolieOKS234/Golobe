c5060393b0fe40e979224ea90753b054adc9345d
OkolieOKS234 <dokolie040@gmail.com>
2023-05-25 13:43:25 +0100

    first

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Hero from './components/homepage/Hero'
+
+function App() {
+  
+
+  return (
+    <>
+     <Hero/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/homepage/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import HeroImage from "../../assets/hero_travel.png"
+
+const Hero = () => {
+  return (
+    <header className='header text-center p-24 rounded-lg'>
+
+<ul>
+    <li></li>
+    <li></li>
+</ul>
+<p>Hello</p>
+    </header>
+  )
+}
+
+export default Hero
