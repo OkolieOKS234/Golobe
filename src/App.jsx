87c3f5939db674b5a3ff21268ad3697ea2c12bf3
OkolieOKS234 <dokolie040@gmail.com>
2023-05-29 14:19:10 +0100

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,18 @@ import Footer from './components/homepage/Footer'
 import Hero from './components/homepage/Hero'
 import Reviews from './components/homepage/Reviews'
 import Trip from './components/homepage/Trip'
-import Login from './pages/Login'
+
 
 function App() {
   
 
   return (
     <>
-     {/* <Hero/>
+     <Hero/>
      <Trip/>
      <Reviews/>
-     <Footer/> */}
-     <Login/>
+     <Footer/>
+     
     </>
   )
 }
